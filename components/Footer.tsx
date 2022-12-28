238118fe7a65595e697d2dbb2e0d62eeb411f995
advwastaken <advwastaken@protonmail.com>
2022-12-28 19:10:42 +0530

    *

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer: NextPage = () => {
   return (
     <div className="h-auto p-2 w-full flex items-center justify-center flex-col">
       <div className="h-20 w-full"></div>
+        <Collapse.Group>
       <Collapse title="Whats your location?">
-        <div className="h-4"></div>
-        <Text>India 🇮🇳<br />{"‎ ".repeat(100)}</Text>
+        <Text>
+            India 🇮🇳{"‎ ".repeat(90)}
+        </Text>
       </Collapse>
-      <div className="h-8 w-full"></div>
       <Collapse title="Whats your socials?">
-        <div className="h-8 w-full"></div>
-        <Popover>
+      <Popover>
           <Popover.Trigger>
             <Button shadow color="primary">
               Email
@@ -23,11 +23,10 @@ const Footer: NextPage = () => {
             <Text css={{ p: "$10" }}>mishraarush5655@gmail.com</Text>
           </Popover.Content>
         </Popover>
-        <Text>{"‎ ".repeat(100)}</Text>
-        <div className="h-4"></div>
       </Collapse>
+    </Collapse.Group>
       <div className="h-20 w-full"></div>
-      <div className="h-12 w-full"></div>
+      <div className="h-4 w-full"></div>
       <Text h6>©Arush 2022</Text>
     </div>
   );
